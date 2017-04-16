925b0eea27fc9bfda11f69de2fa4489773689f06
juliascript <juliageist23@gmail.com>
2017-04-16 00:19:49 -0700

    naive spider for fake news archive

--- fake_news/spiders/fake.py
+++ fake_news/spiders/fake.py
@@ -9,20 +9,31 @@ class FakeSpider(scrapy.Spider):
 
     def parse(self, response):
         links = response.xpath('//*[@class="url"]/a/@href').extract()
-        print links
+        # print links
 
-        # for link in links:
-        # 	# go to that page and scrape all data from each site, 
-        # 	# for now we can look at only the most recent archive
-        # 	yield scrapy.http.Request(link, callback=self.parse_wayback)
-        yield scrapy.http.Request(links[0], callback=self.parse_wayback)
+        for link in links:
+            # go to that page and scrape all data from each site, 
+            # for now we can look at only the most recent archive
+            yield scrapy.http.Request(link, callback=self.parse_wayback)
+
+        # yield scrapy.http.Request(links[0], callback=self.parse_wayback)
 
     def parse_wayback(self, response):
-    	list_of_archives = response.xpath('//*[@class="mainBody"]/a/@href').extract()
+        list_of_archives = response.xpath('//*[@class="mainBody"]/a/@href').extract()
 
-    	# for archive_link in list_of_archives:
-    	# 	yield scrapy.http.Request(archive_link, callback=self.parse_wayback_archive)
-    	yield scrapy.http.Request(list_of_archives[0], callback=self.parse_wayback_archive)
+        # print list_of_archives
+
+        for archive_link in list_of_archives:
+            archive_link = 'http:' + archive_link
+            yield scrapy.http.Request(archive_link, callback=self.parse_wayback_archive)
+        
+        # yield scrapy.http.Request(url, callback=self.parse_wayback_archive)
 
     def parse_wayback_archive(self, response):
-    	pass
+        yield {
+            'headlines': response.xpath('//h2/a/text()').extract_first()
+        } 
+
+
+
+
