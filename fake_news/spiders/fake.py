a43fae4a5b0ebd104e3f0040f3afd48b77dbcb77
juliascript <juliageist23@gmail.com>
2017-04-15 18:56:36 -0700

    for each domain in wayback archive, visit each and grab every archived version of the site url

--- fake_news/spiders/fake.py
+++ fake_news/spiders/fake.py
@@ -4,15 +4,25 @@ import scrapy
 
 class FakeSpider(scrapy.Spider):
     name = "fake"
-    allowed_domains = ["archive-it.org/collections/8142/?show=Sites"]
+    allowed_domains = ["archive-it.org", "wayback.archive-it.org"]
     start_urls = ['https://www.archive-it.org/collections/8142/?show=Sites/']
 
     def parse(self, response):
         links = response.xpath('//*[@class="url"]/a/@href').extract()
         print links
 
-        for link in links:
-        	# go to that page and scrape all data from each site, 
-        	# for now we can look at only the most recent archive
-        	# yield scrapy.http.Request(link)
-        	pass
+        # for link in links:
+        # 	# go to that page and scrape all data from each site, 
+        # 	# for now we can look at only the most recent archive
+        # 	yield scrapy.http.Request(link, callback=self.parse_wayback)
+        yield scrapy.http.Request(links[0], callback=self.parse_wayback)
+
+    def parse_wayback(self, response):
+    	list_of_archives = response.xpath('//*[@class="mainBody"]/a/@href').extract()
+
+    	# for archive_link in list_of_archives:
+    	# 	yield scrapy.http.Request(archive_link, callback=self.parse_wayback_archive)
+    	yield scrapy.http.Request(list_of_archives[0], callback=self.parse_wayback_archive)
+
+    def parse_wayback_archive(self, response):
+    	pass
