74ce0ed1ddad929c71a2825fdd5985af223c523e
juliascript <juliageist23@gmail.com>
2017-04-16 00:28:36 -0700

    rm old comments

--- fake_news/spiders/fake.py
+++ fake_news/spiders/fake.py
@@ -9,27 +9,20 @@ class FakeSpider(scrapy.Spider):
 
     def parse(self, response):
         links = response.xpath('//*[@class="url"]/a/@href').extract()
-        # print links
 
         for link in links:
-            # go to that page and scrape all data from each site, 
-            # for now we can look at only the most recent archive
+            # go to that page and scrape data from each site
             yield scrapy.http.Request(link, callback=self.parse_wayback)
-
-        # yield scrapy.http.Request(links[0], callback=self.parse_wayback)
 
     def parse_wayback(self, response):
         list_of_archives = response.xpath('//*[@class="mainBody"]/a/@href').extract()
-
-        # print list_of_archives
 
         for archive_link in list_of_archives:
             archive_link = 'http:' + archive_link
             yield scrapy.http.Request(archive_link, callback=self.parse_wayback_archive)
         
-        # yield scrapy.http.Request(url, callback=self.parse_wayback_archive)
-
     def parse_wayback_archive(self, response):
+        # only grabbing the first headline (in a naive way)
         yield {
             'headlines': response.xpath('//h2/a/text()').extract_first()
         } 
